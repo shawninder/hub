3b1fff6d45ec3d780f7864428516fe600613cace
Shawn Inder <shawninder@gmail.com>
2018-06-20 13:29:57 -0400

    clean up logs

--- handleSlice.js
+++ handleSlice.js
@@ -3,7 +3,9 @@ const log = require('./log')
 module.exports = exports = function handleSlice ({ client, parties }) {
   return (action) => {
     action.name_lc = action.name.toLowerCase()
-    log(`Host "${action.socketKey}" dispatched`, action)
+    if (action.type !== 'Party:slice') {
+      log(`Host "${action.socketKey}" dispatched`, action)
+    }
     const reject = (msg) => {
       const err = {
         msg,
@@ -14,15 +16,17 @@ module.exports = exports = function handleSlice ({ client, parties }) {
     }
     const party = parties[action.name_lc]
     if (party) {
-      const guestKeys = Object.keys(party.guests)
       if (party.host.key === action.socketKey) {
+        const guestKeys = Object.keys(party.guests)
         party.state = action.slice
         guestKeys.forEach((guestKey) => {
           const guest = party.guests[guestKey]
           log(`Emitting 'slice' to guest "${guestKey}"`, action.slice)
           guest.emit('slice', action.slice)
         })
-        log(`reached "${guestKeys.length}" guests`)
+        if (guestKeys.length > 0) {
+          log(`reached "${guestKeys.length}" guests`)
+        }
       } else {
         reject("Can't send state slice, you're not the host!")
       }
