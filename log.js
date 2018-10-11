b10a8d7bab29644ac77ae272156c4b04447adf14
Shawn Inder <shawninder@gmail.com>
2018-10-11 02:51:20 -0400

    specify mongo cluster

--- log.js
+++ log.js
@@ -1,18 +1,22 @@
 const Data = require('data')
 let username
 let password
+let cluster
 let hosts
 let databaseName
 let replicaSet
+
 if (process.env.NODE_ENV === 'production') {
   username = process.env.ATLAS_HUB_USERNAME
   password = process.env.ATLAS_HUB_PASSWORD
+  cluster = process.env.ATLAS_CLUSTER
   hosts = process.env.ATLAS_HOSTS
   databaseName = process.env.ATLAS_DATABASE
   replicaSet = process.env.ATLAS_REPLICA_SET
 } else {
   username = process.env.MONGO_HUB_USERNAME
   password = process.env.MONGO_HUB_PASSWORD
+  cluster = process.env.MONGO_CLUSTER
   hosts = process.env.MONGO_HOSTS
   databaseName = process.env.MONGO_DATABASE
   replicaSet = process.env.MONGO_REPLICA_SET
@@ -20,6 +24,7 @@ if (process.env.NODE_ENV === 'production') {
 const data = new Data({
   username,
   password,
+  cluster,
   hosts,
   databaseName,
   replicaSet
